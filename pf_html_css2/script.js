20f8f8f92f02ac536b4257fcb695daccbefd9733
maenoyudai <157856610+maenoyudai@users.noreply.github.com>
2024-05-14 21:54:41 +0900

    モーダル

--- pf_html_css2/script.js
+++ pf_html_css2/script.js
@@ -23,3 +23,35 @@ window.addEventListener('scroll', () => {
         $('#header').removeClass('active');
     }
 });
+
+// モーダル
+const btn = document.querySelectorAll(".modal-toggle");
+btn.forEach(function (btn) {
+btn.onclick = function () {
+const modalAttribute = btn.getAttribute('data-modal');
+const modalShow = document.getElementById(modalAttribute);
+modalShow.classList.add('show');
+};
+});
+const closeBtnTop = document.querySelectorAll(".close-btn-top");
+closeBtnTop.forEach(function (btn) {
+btn.onclick = function () {
+const modal = btn.closest('.modal-outer');
+modal.classList.remove('show');
+};
+});
+const closeBtnBottom = document.querySelectorAll(".close-btn-bottom");
+closeBtnBottom.forEach(function (btn) {
+btn.onclick = function () {
+const modal = btn.closest('.modal-outer');
+modal.classList.remove('show');
+};
+});
+const modalOuter = document.querySelectorAll('.modal-outer');
+modalOuter.forEach(function (outer) {
+outer.onclick = function () {
+if(event.target.closest('.modal-inner') === null) {
+event.target.classList.remove('show');
+}
+}
+});
